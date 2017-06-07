e17ab88e0f955ec8e7d43c205090fa62b76eb35d
pooyabr <pberahmandi2@horizon.csueastbay.edu>
2017-06-07 15:01:10 -0700

    Upload

---
+++ js/second.js
@@ -0,0 +1,24 @@
+/*function containpic() {
+  var h = window.innerHeight;
+  var w = window.innerWidth;
+  var pic = document.getElementsByClassName("biggest");
+  if (pic.width > pic.height) {
+    document.getElementsByClassName('biggest').setAttribute("width", w);
+    if (pic.height > h) {
+      document.getElementsByClassName('biggest').setAttribute("width", i);
+      i = i++;
+    }
+
+
+
+
+  }
+
+}
+
+console.log("W " w);
+console.log("pic " pic);
+console.log("h " + h);
+console.log("biggest " + biggest);
+console.log();
+*/
